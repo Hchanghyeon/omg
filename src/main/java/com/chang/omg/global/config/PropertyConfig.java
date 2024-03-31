0b27ce91f48380b750f73e500bdb3b537aeb8f57
changhyeon <changhyeon.h@kakao.com>
2024-03-31 19:03:14 +0900

    feat: KartRider Properties 추가 및 공통 속성 분리

--- src/main/java/com/chang/omg/global/config/PropertyConfig.java
+++ src/main/java/com/chang/omg/global/config/PropertyConfig.java
@@ -4,10 +4,17 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 import com.chang.omg.global.config.property.CorsProperties;
+import com.chang.omg.global.config.property.KartRiderProperties;
 import com.chang.omg.global.config.property.MapleStoryMProperties;
+import com.chang.omg.global.config.property.NexonCommonProperties;
 
 @Configuration
-@EnableConfigurationProperties(value = {MapleStoryMProperties.class, CorsProperties.class})
+@EnableConfigurationProperties(value = {
+        CorsProperties.class,
+        MapleStoryMProperties.class,
+        KartRiderProperties.class,
+        NexonCommonProperties.class
+})
 public class PropertyConfig {
 
 }
