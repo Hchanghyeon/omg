378450d7b0ac444f28ca0119612928005592edba
changhyeon <changhyeon.h@kakao.com>
2024-07-12 20:44:20 +0900

    refactor: Service 코드 변경 및 예외 처리 일부 로직 변경

--- src/main/java/com/chang/omg/application/game/GameService.java
+++ src/main/java/com/chang/omg/application/game/GameService.java
@@ -1,6 +1,8 @@
 package com.chang.omg.application.game;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ import com.chang.omg.domains.game.domain.GameCharacterSearchLog;
 import com.chang.omg.domains.game.domain.GameType;
 import com.chang.omg.domains.rank.domain.CharacterInfo;
 import com.chang.omg.domains.rank.repository.GameRankingRedisRepository;
+import com.chang.omg.global.exception.ApiException;
+import com.chang.omg.global.exception.ApiExceptionCode;
 import com.chang.omg.infrastructure.api.kartrider.KartRiderApi;
 import com.chang.omg.infrastructure.api.kartrider.dto.User;
 import com.chang.omg.infrastructure.api.kartrider.dto.UserBasic;
@@ -25,7 +29,10 @@ import com.chang.omg.presentation.game.dto.KartRiderUserInfoResponse;
 import com.chang.omg.presentation.game.dto.MapleStoryMCharacterInfoResponse;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GameService {
@@ -39,31 +46,58 @@ public class GameService {
             final String characterName,
             final String worldName
     ) {
-        final Character character = mapleStoryMApi.getCharacterOcid(characterName, worldName);
-        final CharacterBasic characterBasic = mapleStoryMApi.getCharacterBasic(character.ocid());
-        final CharacterItemEquipment characterItemEquipment = mapleStoryMApi.getCharacterItem(character.ocid());
-        final CharacterStat characterStat = mapleStoryMApi.getCharacterStat(character.ocid());
-        final CharacterGuild characterGuild = mapleStoryMApi.getCharacterGuild(character.ocid());
+        final Character character = mapleStoryMApi.getCharacterOcid(characterName, worldName); // Blocking
 
         saveGameCharacterSearchLog(GameType.MAPLESTORYM, worldName, characterName);
 
-        return MapleStoryMCharacterInfoResponse.of(
-                characterBasic,
-                characterItemEquipment,
-                characterStat,
-                characterGuild
-        );
+        final CompletableFuture<MapleStoryMCharacterInfoResponse> future = CompletableFuture.supplyAsync(() -> {
+            final Mono<CharacterBasic> characterBasic = mapleStoryMApi.getCharacterBasic(character.ocid());
+            final Mono<CharacterItemEquipment> characterItemEquipment = mapleStoryMApi.getCharacterItem(
+                    character.ocid());
+            final Mono<CharacterStat> characterStat = mapleStoryMApi.getCharacterStat(character.ocid());
+            final Mono<CharacterGuild> characterGuild = mapleStoryMApi.getCharacterGuild(character.ocid());
+
+            return Mono.zip(characterBasic, characterItemEquipment, characterStat, characterGuild)
+                    .map(tuple -> MapleStoryMCharacterInfoResponse.of(
+                            tuple.getT1(),
+                            tuple.getT2(),
+                            tuple.getT3(),
+                            tuple.getT4()
+                    ))
+                    .toFuture()
+                    .join(); // Blocking on the combined future to get the final result
+        });
+
+        try {
+            return future.get();
+        } catch (ExecutionException | InterruptedException exception) {
+            log.error("ErrorTrace", exception);
+            throw new ApiException(ApiExceptionCode.API_UNKNOWN_ERROR);
+        }
     }
 
     public KartRiderUserInfoResponse getKartRiderUserInfo(final String userName) {
         final User user = kartRiderApi.getUserOuid(userName);
         final String ouid = user.ouidInfo().get(0).ouid();
-        final UserBasic userBasic = kartRiderApi.getUserBasic(ouid);
-        final UserTitleEquipment userTitleEquipment = kartRiderApi.getUserTitleEquipment(ouid);
 
-        saveGameCharacterSearchLog(GameType.KARTRIDER, null, userBasic.racerName());
+        saveGameCharacterSearchLog(GameType.KARTRIDER, null, userName);
 
-        return KartRiderUserInfoResponse.of(userBasic, userTitleEquipment);
+        final CompletableFuture<KartRiderUserInfoResponse> future = CompletableFuture.supplyAsync(() -> {
+            final Mono<UserBasic> userBasic = kartRiderApi.getUserBasic(ouid);
+            final Mono<UserTitleEquipment> userTitleEquipment = kartRiderApi.getUserTitleEquipment(ouid);
+
+            return Mono.zip(userBasic, userTitleEquipment)
+                    .map(tuple -> KartRiderUserInfoResponse.of(tuple.getT1(), tuple.getT2()))
+                    .toFuture()
+                    .join();
+        });
+
+        try {
+            return future.get();
+        } catch (ExecutionException | InterruptedException exception) {
+            log.error("ErrorTrace", exception);
+            throw new ApiException(ApiExceptionCode.API_UNKNOWN_ERROR);
+        }
     }
 
     private void saveGameCharacterSearchLog(
