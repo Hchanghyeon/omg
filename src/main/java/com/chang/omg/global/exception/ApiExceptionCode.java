126844a670e89b473fb44cf971a0e2ca1a236708
changhyeon <changhyeon.h@kakao.com>
2024-07-12 20:44:20 +0900

    refactor: Service 코드 변경 및 예외 처리 일부 로직 변경

--- src/main/java/com/chang/omg/global/exception/ApiExceptionCode.java
+++ src/main/java/com/chang/omg/global/exception/ApiExceptionCode.java
@@ -12,10 +12,10 @@ import lombok.RequiredArgsConstructor;
 public enum ApiExceptionCode implements ExceptionCode {
     API_INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, "API-001", "게임사 서버에 에러가 발생하였습니다."),
     API_BAD_REQUEST(BAD_REQUEST, "API-002", "요청 정보가 잘못되었습니다."),
-    API_FORBIDDEN(FORBIDDEN, "API-003", "요청 권한이 없습니다."),
+    API_FORBIDDEN(FORBIDDEN, "API-003", "요청 권한이 없경습니다."),
     API_TOO_MANY_REQUESTS(TOO_MANY_REQUESTS, "API_004", "요청이 너무 많습니다."),
     API_GAME_TYPE_IS_INVALID(BAD_REQUEST, "API_005", "게임 타입이 유효하지 않습니다."),
-    ;
+    API_UNKNOWN_ERROR(INTERNAL_SERVER_ERROR, "API-006", "알 수 없는 에러가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/chang/omg/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/chang/omg/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.chang.omg.global.exception;
 
 import static com.chang.omg.global.exception.GlobalExceptionCode.*;
 
+import java.util.Arrays;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +17,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(final ApiException apiException) {
         log.error("외부 API 요청 에러: {}", apiException.getMessage(), apiException);
+        log.error(Arrays.toString(apiException.getRejectedValues()));
 
         return ResponseEntity.status(apiException.getExceptionCode().getStatus())
                 .body(new ErrorResponse(
