e9d062ae64ea6a058ce3f0ed07128e0685d694d0
changhyeon <changhyeon.h@kakao.com>
2024-07-25 17:05:41 +0900

    feat: Redis를 이용한 사용자 회원가입 인증번호 기능 구현

--- src/main/java/com/chang/omg/domain/member/controller/MemberController.java
+++ src/main/java/com/chang/omg/domain/member/controller/MemberController.java
@@ -2,12 +2,17 @@ package com.chang.omg.domain.member.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.chang.omg.domain.member.controller.dto.MemberCreateRequest;
-import com.chang.omg.domain.member.controller.dto.MemberCreateResponse;
+import com.chang.omg.domain.member.controller.dto.request.MemberAuthCodeRequest;
+import com.chang.omg.domain.member.controller.dto.request.MemberCreateRequest;
+import com.chang.omg.domain.member.controller.dto.request.MemberVerificationRequest;
+import com.chang.omg.domain.member.controller.dto.response.MemberCreateResponse;
+import com.chang.omg.domain.member.controller.dto.response.MemberVeriticationStatusResponse;
 import com.chang.omg.domain.member.service.MemberService;
 
 import jakarta.validation.Valid;
@@ -22,9 +27,29 @@ public class MemberController {
 
     @PostMapping
     public ResponseEntity<MemberCreateResponse> createMember(
-            @RequestBody @Valid final MemberCreateRequest memberCreateRequest) {
+            @RequestBody @Valid final MemberCreateRequest memberCreateRequest
+    ) {
         final Long memberId = memberService.createMember(memberCreateRequest);
 
         return ResponseEntity.ok(new MemberCreateResponse(memberId));
+    }
+
+    @PostMapping("/verification-auth-code")
+    public ResponseEntity<Void> createAuthCodeAndSendEmail(
+            @RequestBody final MemberAuthCodeRequest memberAuthCodeRequest
+    ) {
+        memberService.createAuthCodeAndSendEmail(memberAuthCodeRequest);
+
+        return ResponseEntity.noContent()
+                .build();
+    }
+
+    @GetMapping("/verification-auth-code")
+    public ResponseEntity<MemberVeriticationStatusResponse> verifyAuthCode(
+            @ModelAttribute MemberVerificationRequest memberVerificationRequest
+    ) {
+        final boolean isVerification = memberService.verifyAuthCode(memberVerificationRequest);
+
+        return ResponseEntity.ok(new MemberVeriticationStatusResponse(isVerification));
     }
 }
---
+++ src/main/java/com/chang/omg/domain/member/domain/MemberRedisRepository.java
@@ -0,0 +1,51 @@
+package com.chang.omg.domain.member.domain;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+import com.chang.omg.domain.member.exception.MemberException;
+import com.chang.omg.domain.member.exception.MemberExceptionCode;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class MemberRedisRepository {
+
+    private static final String PREFIX_AUTH_CODE = "AUTH:";
+    private static final int AUTH_CODE_TTL = 120;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private ValueOperations<String, Object> valueOperations;
+
+    @PostConstruct
+    public void initialize() {
+        valueOperations = redisTemplate.opsForValue();
+    }
+
+    public void saveAuthCodeWithEmail(final int authCode, final String memberEmail) {
+        valueOperations.set(PREFIX_AUTH_CODE + memberEmail, authCode, AUTH_CODE_TTL, TimeUnit.SECONDS);
+    }
+
+    public boolean existsAuthCodeByEmail(final String memberEmail) {
+        final Object authCode = valueOperations.get(PREFIX_AUTH_CODE + memberEmail);
+
+        return !Objects.isNull(authCode);
+    }
+
+    public int findAuthCodeByEmail(final String memberEmail) {
+        final Object authCode = valueOperations.get(PREFIX_AUTH_CODE + memberEmail);
+
+        if (Objects.isNull(authCode)) {
+            throw new MemberException(MemberExceptionCode.MEMBER_AUTH_CODE_NOT_FOUND, memberEmail);
+        }
+
+        return (int)authCode;
+    }
+}
