b677f7bc8e0fe75a0994c568735cb8bdd6aea58c
changhyeon <changhyeon.h@kakao.com>
2024-07-26 14:57:17 +0900

    feat: 인증번호 성공시 RegisterToken 발급 기능과 회원가입시 RegiserToken 인증 기능 구현

--- src/main/java/com/chang/omg/domain/member/controller/MemberController.java
+++ src/main/java/com/chang/omg/domain/member/controller/MemberController.java
@@ -6,13 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.chang.omg.domain.auth.security.JwtTokenExtractor;
+import com.chang.omg.domain.auth.security.JwtTokenProvider;
+import com.chang.omg.domain.auth.security.TokenType;
 import com.chang.omg.domain.member.controller.dto.request.MemberAuthCodeRequest;
 import com.chang.omg.domain.member.controller.dto.request.MemberCreateRequest;
 import com.chang.omg.domain.member.controller.dto.request.MemberVerificationRequest;
 import com.chang.omg.domain.member.controller.dto.response.MemberCreateResponse;
-import com.chang.omg.domain.member.controller.dto.response.MemberVeriticationStatusResponse;
+import com.chang.omg.domain.member.controller.dto.response.MemberRegisterTokenResponse;
+import com.chang.omg.domain.member.exception.MemberException;
+import com.chang.omg.domain.member.exception.MemberExceptionCode;
 import com.chang.omg.domain.member.service.MemberService;
 
 import jakarta.validation.Valid;
@@ -23,12 +29,23 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/members")
 public class MemberController {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenExtractor jwtTokenExtractor;
     private final MemberService memberService;
 
     @PostMapping
     public ResponseEntity<MemberCreateResponse> createMember(
-            @RequestBody @Valid final MemberCreateRequest memberCreateRequest
+            @RequestBody @Valid final MemberCreateRequest memberCreateRequest,
+            @RequestHeader final String authorization
     ) {
+        final String registerToken = jwtTokenExtractor.extractToken(authorization);
+        final String tokenType = jwtTokenProvider.getSubject(registerToken);
+        final String tokenEmail = jwtTokenProvider.getClaim(registerToken, "email");
+
+        if (!tokenType.equals(TokenType.REGISTER.toString()) || !tokenEmail.equals(memberCreateRequest.email())) {
+            throw new MemberException(MemberExceptionCode.MEMBER_NOT_MATCHED_TOKEN_DETAILS);
+        }
+
         final Long memberId = memberService.createMember(memberCreateRequest);
 
         return ResponseEntity.ok(new MemberCreateResponse(memberId));
@@ -45,11 +62,11 @@ public class MemberController {
     }
 
     @GetMapping("/verification-auth-code")
-    public ResponseEntity<MemberVeriticationStatusResponse> verifyAuthCode(
-            @ModelAttribute MemberVerificationRequest memberVerificationRequest
+    public ResponseEntity<MemberRegisterTokenResponse> verifyAuthCode(
+            @ModelAttribute final MemberVerificationRequest memberVerificationRequest
     ) {
-        final boolean isVerification = memberService.verifyAuthCode(memberVerificationRequest);
+        final String registerToken = memberService.verifyAuthCode(memberVerificationRequest);
 
-        return ResponseEntity.ok(new MemberVeriticationStatusResponse(isVerification));
+        return ResponseEntity.ok(new MemberRegisterTokenResponse(registerToken));
     }
 }
--- src/main/java/com/chang/omg/domain/member/exception/MemberExceptionCode.java
+++ src/main/java/com/chang/omg/domain/member/exception/MemberExceptionCode.java
@@ -16,7 +16,10 @@ public enum MemberExceptionCode implements ExceptionCode {
     MEMBER_CREATE_FAIL(BAD_REQUEST, "MEM-002", "사용자의 입력 값이 잘못되었습니다."),
     MEMBER_ALREADY_EXISTS(BAD_REQUEST, "MEM-003", "가입된 사용자가 이미 존재합니다."),
     MEMBER_ALREADY_GET_AUTH_CODE(BAD_REQUEST, "MEM-004", "사용자가 이미 인증요청을 진행한 상태입니다. 시간이 만료된 이후 다시 요청해주세요."),
-    MEMBER_AUTH_CODE_NOT_FOUND(NOT_FOUND, "MEM-005", "사용자의 인증번호가 만료되었거나 존재하지 않습니다.");
+    MEMBER_AUTH_CODE_NOT_FOUND(NOT_FOUND, "MEM-005", "사용자의 인증번호가 만료되었거나 존재하지 않습니다."),
+    MEMBER_AUTH_CODE_NOT_MATCHED(BAD_REQUEST, "MEM-006", "사용자가 보낸 인증번호가 일치하지 않습니다."),
+    MEMBER_NOT_MATCHED_TOKEN_DETAILS(BAD_REQUEST, "MEM-007", "사용자의 회원가입 정보와 토큰 정보가 일치하지 않습니다."),
+    ;
 
     private final HttpStatus status;
     private final String code;
