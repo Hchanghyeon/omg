b0caa4cbf7db6fa788295adb32cd44cd814a1e7d
changhyeon <changhyeon.h@kakao.com>
2024-08-28 20:52:06 +0900

    refactor: MailService 추상화

--- src/main/java/com/chang/omg/domain/mail/service/MailService.java
+++ src/main/java/com/chang/omg/domain/mail/service/MailService.java
@@ -1,26 +1,9 @@
 package com.chang.omg.domain.mail.service;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
 import com.chang.omg.domain.mail.service.dto.EmailDetails;
 
-import lombok.RequiredArgsConstructor;
+public interface MailService {
 
-@Service
-@RequiredArgsConstructor
-public class MailService {
+    void sendEmail(final EmailDetails emailDetails);
 
-    private final JavaMailSender mailSender;
-
-    public void sendEmail(final EmailDetails emailDetails) {
-        final SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(emailDetails.receiver());
-        message.setSubject(emailDetails.subject());
-        message.setText(emailDetails.message());
-
-        mailSender.send(message);
-    }
 }
